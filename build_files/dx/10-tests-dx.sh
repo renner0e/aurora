eb6fd14a6bc0f947e200ffb90684522892e7f627
renner <renner0@posteo.de>
2026-05-08 17:57:08 +0200

    test: add back docker tests
    This was a temporary measure as docker wasn't available for f44 in the
    beginning

--- build_files/dx/10-tests-dx.sh
+++ build_files/dx/10-tests-dx.sh
@@ -6,6 +6,10 @@ set -eoux pipefail
 
 IMPORTANT_PACKAGES_DX=(
     code
+    containerd.io
+    docker-ce
+    docker-buildx-plugin
+    docker-compose-plugin
     flatpak-builder
     libvirt
     podman-bootc
